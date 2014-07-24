4256f003192de0ef2bd30c64d0ace655e9372e99
ashariffvjil <a.shariff@vjil.com>
2014-07-24 15:31:48 +0530

    config changes

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/Minotz456/';
+$config['base_url']	= 'http://172.27.2.107/Minotz/';
 
 /*
 |--------------------------------------------------------------------------
@@ -26,7 +26,7 @@ $config['base_url']	= 'http://localhost/Minotz456/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
